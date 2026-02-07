85b3c91c7ee5f7baf835d1dfcfee1372c04f1fe6
LucasH24 <LucasH122436@gmail.com>
2026-02-07 02:18:03 -0600

    test reversion

--- rankCalc.py
+++ rankCalc.py
@@ -193,9 +193,5 @@ def replace_custom_colors(rank, p, r):
             if component[0] == "%r":
                 component[0] = r
 
-    if new_rank[1][0] == None:
-        new_rank[1][0] = 'fe3f3f'
 
     return new_rank
-
-
